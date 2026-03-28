9b8bbae74729b7b561b3f788062fceebb6dfe744
Afreen <afreee007@gmail.com>
2026-03-28 13:00:22 +0530

    Server.js cors updated

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
 const connectDB = require("./config/db");
 const path = require("path");
 const app = express();
-app.use(cors());
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://your-vercel-app.vercel.app"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 const Role = require("./models/role");
 const State = require("./models/states");
